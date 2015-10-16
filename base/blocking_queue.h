b55f1f781b5835e525062ec42e58530a68570dde
wujiabin <wujiabin@baidu.com>
2015-10-16 23:44:52 +0800

    add base

---
+++ base/blocking_queue.h
@@ -0,0 +1,147 @@
+// @author: Eric(wujiabin@baidu.com)
+
+#ifndef LOG_REPLAYER_BLOCKING_QUEUE_H
+#define LOG_REPLAYER_BLOCKING_QUEUE_H
+
+#include <boost/circular_buffer.hpp>
+#include <deque>
+#include <string>
+#include <vector>
+
+#include "log_utils.h"
+#include "mutex.h"
+
+namespace replayer {
+
+/// copy from muduo
+template<typename T>
+class BlockingQueue
+{
+public:
+    BlockingQueue()
+        : mutex_(),
+          notEmpty_(mutex_),
+          queue_()
+    {
+    }
+
+    void put(const T& x) {
+        MutexLockGuard lock(mutex_);
+        queue_.push_back(x);
+        notEmpty_.Notify(); // wait morphing saves us
+        // http://www.domaigne.com/blog/computing/condvars-signal-with-mutex-locked-or-not/
+    }
+
+    T take() {
+        MutexLockGuard lock(mutex_);
+        // always use a while-loop, due to spurious wakeup
+        while (queue_.empty()) {
+            notEmpty_.Wait();
+        }
+        assert(!queue_.empty());
+        T front(queue_.front());
+        queue_.pop_front();
+        return front;
+    }
+
+    size_t size() const {
+        MutexLockGuard lock(mutex_);
+        return queue_.size();
+    }
+
+private:
+    mutable MutexLock mutex_;
+    Condition         notEmpty_;
+    std::deque<T>     queue_;
+
+    // noncopyable
+    BlockingQueue(const BlockingQueue&);
+    const BlockingQueue& operator=(const BlockingQueue&);
+};
+
+/// copy from muduo
+template<typename T>
+class BoundedBlockingQueue
+{
+public:
+    explicit BoundedBlockingQueue(int maxSize)
+        : mutex_(),
+          notEmpty_(mutex_),
+          notFull_(mutex_),
+          queue_(maxSize)
+    {
+    }
+
+    void put(const T& x) {
+        MutexLockGuard lock(mutex_);
+        while (queue_.full()) {
+            notFull_.Wait();
+        }
+        assert(!queue_.full());
+        queue_.push_back(x);
+        notEmpty_.Notify();
+    }
+
+    T take() {
+        MutexLockGuard lock(mutex_);
+        while (queue_.empty()) {
+            notEmpty_.Wait();
+        }
+        assert(!queue_.empty());
+        T front(queue_.front());
+        queue_.pop_front();
+        notFull_.Notify();
+        return front;
+    }
+
+    // added by Eric
+    int take(int seconds, T& item) {
+        MutexLockGuard lock(mutex_);
+        if (queue_.empty()) {
+            notEmpty_.Wait(seconds);
+            if (queue_.empty()) {
+                return -1;
+            }
+        }
+        assert(!queue_.empty());
+        T front(queue_.front());
+        queue_.pop_front();
+        notFull_.Notify();
+        item = front;
+        return 0;
+    }
+
+    bool empty() const {
+        MutexLockGuard lock(mutex_);
+        return queue_.empty();
+    }
+
+    bool full() const {
+        MutexLockGuard lock(mutex_);
+        return queue_.full();
+    }
+
+    size_t size() const {
+        MutexLockGuard lock(mutex_);
+        return queue_.size();
+    }
+
+    size_t capacity() const {
+        MutexLockGuard lock(mutex_);
+        return queue_.capacity();
+    }
+
+private:
+    mutable MutexLock          mutex_;
+    Condition                  notEmpty_;
+    Condition                  notFull_;
+    boost::circular_buffer<T>  queue_;
+
+    // noncopyable
+    BoundedBlockingQueue(const BoundedBlockingQueue&);
+    const BoundedBlockingQueue& operator=(const BoundedBlockingQueue&);
+};
+
+} // end replayer namespace
+
+#endif
---
+++ base/mutex.h
@@ -0,0 +1,106 @@
+// @author: Eric(wujiabin@baidu.com)
+
+#ifndef LOG_REPLAYER_MUTEX_H
+#define LOG_REPLAYER_MUTEX_H
+
+#include <pthread.h>
+
+namespace replayer {
+
+/// A simple pthread mutex wrapper
+class MutexLock {
+public:
+    MutexLock() {
+        pthread_mutexattr_t attr;
+        pthread_mutexattr_init(&attr);
+        pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK_NP);
+        pthread_mutex_init(&m_mutex, &attr);
+    }
+
+    ~MutexLock() {
+        pthread_mutex_destroy(&m_mutex);
+    }
+
+    void Lock() {
+        pthread_mutex_lock(&m_mutex);
+    }
+
+    void Unlock() {
+        pthread_mutex_unlock(&m_mutex);
+    }
+
+    /// just for internal use
+    pthread_mutex_t* GetMutex() {
+        return &m_mutex;
+    }
+private:
+    pthread_mutex_t m_mutex;
+
+    // noncopyable
+    MutexLock(const MutexLock&);
+    const MutexLock& operator=(const MutexLock&);
+};
+
+
+/// MutexGuard RAII mutex guard
+class MutexLockGuard {
+public:
+    explicit MutexLockGuard(MutexLock& mutex)
+        : m_mutex(mutex) {
+        m_mutex.Lock();
+    }
+
+    ~MutexLockGuard() {
+        m_mutex.Unlock();
+    }
+
+private:
+    MutexLock& m_mutex;
+
+    // noncopyable
+    MutexLockGuard(const MutexLockGuard&);
+    const MutexLockGuard& operator=(const MutexLockGuard&);
+};
+
+/// pthread condition wrapper
+class Condition {
+public:
+    explicit Condition(MutexLock& mutex)
+        : m_mutex(mutex) {
+        pthread_cond_init(&m_cond, NULL);
+    }
+
+    void Wait() {
+        pthread_cond_wait(&m_cond, m_mutex.GetMutex());
+    }
+
+    void Wait(int seconds) {
+		struct timeval now;
+		struct timespec outtime;
+		gettimeofday(&now, NULL);
+		outtime.tv_sec = now.tv_sec + seconds;
+		outtime.tv_nsec = now.tv_usec * 1000;
+		pthread_cond_timedwait(&m_cond, m_mutex.GetMutex(), &outtime);
+	}
+
+	void Notify() {
+		pthread_cond_signal(&m_cond);
+    }
+
+    void NotifyAll() {
+        pthread_cond_broadcast(&m_cond);
+    }
+
+private:
+    pthread_cond_t m_cond;
+    MutexLock& m_mutex;
+    // noncopyable
+    Condition(const Condition&);
+    const Condition& operator=(const Condition&);
+};
+
+} // end replayer namespace
+
+# define MutexLockGuard(x) static_assert(false, "Missing mutex guard var name")
+
+#endif
