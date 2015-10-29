82c54c04e9b6551add313a04512c081ca937e7cb
wujiabin <wujiabin@baidu.com>
2015-10-29 14:32:32 +0800

    update

--- ip_lib/test.py
+++ ip_lib/test.py
@@ -2,9 +2,14 @@ import ip_lib
 ip_lib.load_dict("dict/GIP-lib-latest.txt")
 
 print ip_lib.get_country("180.214.232.50")
-raw_input("xxx")
+#raw_input("xxx")
 print ip_lib.cleanup()
-raw_input("xxx")
+#raw_input("xxx")
 print ip_lib.get_country("180.214.232.50")
 ip_lib.load_dict("dict/GIP-lib-latest.txt")
 print ip_lib.get_country("180.214.232.50")
+
+ip = ip_lib.IP2Location("dict/GIP-lib-latest.txt")
+print ip.get_country("180.214.232.50")
+print ip.cleanup()
+print ip.get_country("180.214.232.50")
